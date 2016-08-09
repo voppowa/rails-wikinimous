1dc9df9944aaa3b5e4a7d14abbea2ecb9fc6e2e8
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-09 17:16:28 +0300

    finished Wikinimous users sign up login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  resources :articles
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require "faker"
+
+Article.destroy_all
+
+10.times do
+  Article.create(
+    title: Faker::Book.title.to_s,
+    text: Faker::Lorem.paragraph(10).to_s,
+  )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
